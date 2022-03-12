c1d107327ce30e0ad1b2cf685ebd92d91d281bef
Lakhdar <meghrabilakdar@gmail.com>
2022-03-12 23:11:08 +0100

    Second commit

--- src/Assignment02.java
+++ src/Assignment02.java
@@ -5,81 +5,57 @@ import static java.util.stream.Collectors.toList;
 
 public class Assignment02 {
 
-       public  static int minimumDistance(List<Integer> a)
+       public  static int minimumDistances (Integer [] a)
         {
+            HashMap<Integer, Integer> hmap = new HashMap<>();
 
-            // Create a HashMap to
-            // store (key, values) pair.
-            HashMap<Integer, Integer> hmap
-                    = new HashMap<>();
             int minDistance = Integer.MAX_VALUE;
 
-            // Initialize previousIndex
-            // and currentIndex as 0
-            int previousIndex = 0, currentIndex = 0;
+            int prevIndex = 0, currentIndex = 0;
 
-            // Traverse the array and
             // find the minimum distance
-            // between the same elements with map
-            for (int i = 0; i < a.size(); i++) {
 
-                if (hmap.containsKey(a.get(i))) {
+            for (int i = 0; i < a.length; i++) {
+
+                if (hmap.containsKey(a[i])) {
                     currentIndex = i;
 
                     // Fetch the previous index from map.
-                    previousIndex = hmap.get(a.get(i));
+                    prevIndex = hmap.get(a[i]);
 
                     // Find the minimum distance.
-                    minDistance
-                            = Math.min(
-                            (currentIndex - previousIndex),
-                            minDistance);
+                    minDistance = Math.min((currentIndex - prevIndex), minDistance);
                 }
 
-                // Update the map.
-                hmap.put(a.get(i), i);
+                hmap.put(a[i], i);
             }
 
-            // return minimum distance,
-            // if no such elements found, return -1
+            // return minimum distance, if no such elements found, return -1
             return (
-                    minDistance == Integer.MAX_VALUE
-                            ? -1
-                            : minDistance);
+                    minDistance == Integer.MAX_VALUE ? -1 : minDistance);
         }
 
 
+
     public static void main(String[] args) throws IOException {
+
         BufferedReader bufferedReader = new BufferedReader(new FileReader("Assignment02-1.txt"));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("out.txt"));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("Resultat02.txt"));
 
         int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+        List <Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
                 .map(Integer::parseInt)
                 .collect(toList());
+        Integer[] array = a.toArray(new Integer[a.size()]);
 
-        int result = Assignment02.minimumDistance(a);
+        int result = Assignment02.minimumDistances ( array);
+        System.out.println("The minimum distance is  "+result);
 
-        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.write( String.valueOf(result));
         bufferedWriter.newLine();
 
         bufferedReader.close();
         bufferedWriter.close();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
